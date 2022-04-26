0cd22cad53f0f56b59ebf524f773b378e94cedb1
suryamathur5161 <suryamathur5161@github.com>
2022-04-26 12:25:09 +0530

    Copy Paste Added

--- app/src/main/java/com/example/myreelloader/MainActivity.java
+++ app/src/main/java/com/example/myreelloader/MainActivity.java
@@ -3,14 +3,10 @@ package com.example.myreelloader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -34,42 +30,28 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences=getSharedPreferences("Caption",MODE_PRIVATE);
         String val=sharedPreferences.getString("Cap","Nothing");
         caption.setText(val);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Instagram.download(MainActivity.this,editText.getText().toString());
-            }
+        button.setOnClickListener(v -> Instagram.download(MainActivity.this,editText.getText().toString()));
+
+        paste.setOnClickListener(v -> {
+            ClipData a=clipboardManager.getPrimaryClip();
+            ClipData.Item item=a.getItemAt(0);
+            String text=item.getText().toString();
+            editText.setText(text);
         });
 
-        paste.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ClipData a=clipboardManager.getPrimaryClip();
-                ClipData.Item item=a.getItemAt(0);
-                String text=item.getText().toString();
-                editText.setText(text);
-            }
+        save.setOnClickListener(v -> {
+            String msg=caption.getText().toString();
+            SharedPreferences sharedPreferences1 =getSharedPreferences("Caption",MODE_PRIVATE);
+            SharedPreferences.Editor editor= sharedPreferences1.edit();
+            editor.putString("Cap",msg);
+            editor.apply();
+            caption.setText(msg);
         });
 
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String msg=caption.getText().toString();
-                SharedPreferences sharedPreferences=getSharedPreferences("Caption",MODE_PRIVATE);
-                SharedPreferences.Editor editor=sharedPreferences.edit();
-                editor.putString("Cap",msg);
-                editor.apply();
-                caption.setText(msg);
-            }
-        });
-
-        copy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ClipData clipdata=ClipData.newPlainText("Captions",caption.getText().toString());
-                clipboardManager.setPrimaryClip(clipdata);
-                Toast.makeText(MainActivity.this, "Copied", Toast.LENGTH_SHORT).show();
-            }
+        copy.setOnClickListener(v -> {
+            ClipData clipdata=ClipData.newPlainText("Captions",caption.getText().toString());
+            clipboardManager.setPrimaryClip(clipdata);
+            Toast.makeText(MainActivity.this, "Copied", Toast.LENGTH_SHORT).show();
         });
     }
 
